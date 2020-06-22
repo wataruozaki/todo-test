6a8b5992b4edf2ccdc7035db1f1f95e7555a36fd
wataru <dossjakapoko@gmail.com>
2020-06-22 18:16:38 +0900

    formの作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import {
+  Container,
+  Form,
+  InputGroup,
+  Input,
+  InputGroupAddon,
+  Button,
+} from "reactstrap";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [todos, setTodo] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(value);
+  };
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, text];
+    setTodo(newTodos);
+    console.log(newTodos);
+  };
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Container>
+        <h1 className="mt-4">Todo リスト</h1>
+        <Form onSubmit={handleSubmit}>
+          <InputGroup>
+            <Input
+              type="text"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
+            <InputGroupAddon addonType="append">
+              <Button type="submit" color="primary">
+                追加
+              </Button>
+            </InputGroupAddon>
+          </InputGroup>
+        </Form>
+      </Container>
     </div>
   );
 }
